147781eb41761c291b8fa980dff120b2d35c4870
Dormowa <dormowa.s.sherman@gmail.com>
2025-07-16 14:55:17 -0400

    Done

---
+++ exercise-variables.py
@@ -0,0 +1,58 @@
+# 1
+onomatopoeia = 'pop'
+print(onomatopoeia)
+
+# 2
+lyrics = """Katie Casey was baseball mad,
+Had the fever and had it bad.
+Just to root for the home town crew,
+Ev'ry sou
+Katie blew.
+On a Saturday her young beau
+Called to see if she'd like to go
+To see a show, but Miss Kate said "No,
+I'll tell you what you can do:"""
+print(lyrics)
+
+# 3
+gpa = 3.75
+print(type(gpa))
+
+# 4
+empty_var = None
+print(type(empty_var))
+
+# 5
+big_result = 828**282
+print(big_result)
+
+# 6
+expression_result = (52 - 52) + (64/8) +(42 % 8)
+print(expression_result)
+
+# 7
+true = True
+isTrue = (true is not False)
+print(isTrue)
+
+# 8
+weird_word = 'Weird'
+is_match = (weird_word == 'weird')
+print(is_match)
+
+# 9
+# my_variable = false
+# NameError: name 'false' is not defined. Did you mean: 'False'?
+
+# 10
+variable1 = 35/7
+print(variable1)
+
+variable2 = 15**3
+print(variable2)
+
+variable3 = variable2%variable1
+print(variable3)
+
+variable4 = variable2/variable1
+print(variable4)
